1942a451e4650bdcd38f8c921c78f63d0a69c165
thw123 <tanhaowen1996@gmail.com>
2019-01-10 19:49:35 +0800

    did't show
    Signed-off-by: thw123 <tanhaowen1996@gmail.com>

--- js/draw.js
+++ js/draw.js
@@ -1,33 +1,74 @@
 var pause = false;
-var num = 0;    //抽奖人数
-var numberarry = new Array(); //存放编号
+var num = 60;    //抽奖人数
+var numberarry = []; //存放编号
+var luck_num = 1;
+var regust = [];
+var interval; //控制器
+var startBtn = document.getElementById(start);
+var flag = false; //判断是否开始
+var run_luck = $('#shownumber');
+var span = document.querySelector('.close');
+var btn_set=document.getElementById("btnset");
 
-function start(){
 
+function start(){     //开始结束抽奖
+    if(numberarry.length < 1){
+        alert("已经全部抽取完毕啦！");
+    }else if(!interval){
+        //console.log("start");
+        startBtn.html("结束");
+        //startBtn.value="jieshu";
+        flag = true;
+        interval = setInterval(function(){
+            let luckn = run_num(numberarry[Math.floor((Math.random()*numberarry.length))]);
+            if(flag == false&&numberarry){
+                add_num(luckn);
+            }
+            if(){
+                clearInterval(interval);
+                flag=false;
+                interval=null;
+                startBtn.html("");
+            }
+        },50);
+    }else if(flag==true){
+        flag = false;
+    }
 }
-function stop(){
-    
+/*function stop(){ //结束
+    flag = false;
+    //alert(luckn);
+    clearInterval(interval);
+    interval = null;
+}*/
+function run_num(number){
+    run_luck.html(number);
+    return number;
 }
-/*$("a.config").click(function(){
-    pause = true;
-})*/
+function add_num(luckn){
+    $('.lucky'.prepend("<li><div class='show_num'>"+luckn+"</div></li>"));
+}
 function showsetbox(){
     var setbox=document.getElementById("set_box"); //获取弹窗
-    var btn_set=document.getElementById("btnset");
     setbox.style.display="block";
     function close(){
     setbox.style.display="none";
     }
 }
 
-function submit(){
+function submit(){  //获取人数
     num = document.getElementById("innumber");
-    for(var i = 0;i < num;i ++){
-        numberarry[i] = i+1;
-    }
-    for(var i = 0;i < num;i ++){
-        window.alert(numberarry[i]);
-    }
+    numberarry = fn(num.value);
+    //alert(numberarry);
+}
+function fn(n){    //初始化
+    var arr=[];
+    (function(){
+        arr.unshift(n);
+        n --;
+        if(n!=0) arguments.callee(n);
+    })();
+    return arr;
 }
 function reset(){
     
@@ -38,7 +79,7 @@ function read(){
 function move(){
     var $shownumber = $("shownumber");
     var interTime = 30;
-    timer = setInterval(function(){
+    //timer = setInterval(function(){
         
-    })
+    //})
 }
