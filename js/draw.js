0d8ccf6ef61fdcdd895c690452116bd29b63e518
thw123 <tanhaowen1996@gmail.com>
2019-01-14 19:59:32 +0800

    xiugai
    Signed-off-by: thw123 <tanhaowen1996@gmail.com>

--- js/draw.js
+++ js/draw.js
@@ -9,11 +9,26 @@ var startBtn = document.getElementById("start");
 var flag = false; //判断是否开始
 var run_luck = document.querySelector('#shownumber');
 var span = document.querySelector('.close');
+var drop_back;
 var btn_set=document.querySelector("#btnset");
 var lownumber = 1;
 let luckn;
 var firstnum=num;
+var luckyn;
+var idn = 0;
 
+document.onkeydown=function(e){
+    if(!e){
+        e = window.event;
+    }
+    if(e.keyCode == 13 && setbox.style.display=="none"){
+        begin();
+    }
+    if(e.keyCode == 13 && setbox.style.display=="block"){
+        submit();
+        close();
+    }
+}
 function begin(){     //开始结束抽奖
     if(numberarry.length < 1){
         alert("已经全部抽取完毕啦！");
@@ -37,20 +52,41 @@ function stop(){ //结束
     //alert(luckn);
     clearInterval(interval);
     interval = null;
+    //showluckn();
     add_num(luckn);
+    console.log(luckn);
     numberarry.remove(luckn);
 }
 function run_num(number){
     run_luck.innerHTML=number;
     return number;
 }
+/*function showluckn(){
+    luckyn=document.getElementById("lucky_circle");
+    luckyn.style.display="block";
+}*/
 function add_num(lucknum){
+    idn ++;
     console.log(lucknum);
-    document.getElementById('luck').innerHTML += "&nbsp;"+"&nbsp;"+"&nbsp;"+"&nbsp;"+lucknum+"</br>";
+    var ul = document.getElementById("luck");
+    var obj=document.createElement("li");
+    obj.innerHTML= "恭喜 "+ lucknum+" 号";
+    obj.id="id"+idn;
+    if(idn == 1){
+        ul.appendChild(obj);
+    }
+    else{
+        var ans="id"+(idn-1);
+        console.log(ans);
+        ul.insertBefore(obj,document.getElementById("ans"));
+    }
+    //document.getElementById('luck').innerHTML += "&nbsp;"+"&nbsp;"+"&nbsp;"+"&nbsp;"+lucknum+"</br>";
     //$('.lucky'.prepend("<li><div class='show_num'>"+luckn+"</div></li>"));
 }
 function showsetbox(){
     setbox=document.getElementById("set_box"); //获取弹窗
+    drop_back=document.getElementById("dropback");
+    drop_back.style.display="block";
     setbox.style.display="block";
 }
 function butOnClick(){
@@ -64,6 +100,7 @@ span.onclick=function(){
 }
 function close(){
     setbox.style.display="none";
+    drop_back.style.display="none";
 }
 Array.prototype.remove=function(val){
     for(let i = 0;i < this.length;i ++){
@@ -75,12 +112,20 @@ Array.prototype.remove=function(val){
 }
 function submit(){  //获取人数
     num = document.getElementById("innumber");
+    if(num.value == ""){
+        alert("请输入人数！");
+        return;
+    }
     firstnum = num.value;
     numberarry = fn(num.value);
+    document.getElementById("luck").innerHTML= null;
+    startBtn.innerHTML="开始";
+    run_luck.innerHTML=0;
     //alert(numberarry);
     close();
 }
 function fn(n){    //初始化
+    idn = 0;
     if(n == 0)
     return 0;
     var arr=[];
@@ -99,25 +144,4 @@ function reset(){
     document.getElementById("luck").innerHTML= null;
     close();
 }
-function launchFullScreen(element) {  
-    if (element.requestFullscreen) {
-       element.requestFullscreen();
-     } else if (element.msRequestFullscreen) {
-       element.msRequestFullscreen();
-     } else if (element.mozRequestFullScreen) {
-       element.mozRequestFullScreen();
-     } else if (element.webkitRequestFullscreen) {
-       element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-     }
- }
- function cancelFullScreen() {  
-    if (document.exitFullscreen) {
-       document.exitFullscreen();
-     } else if (document.msExitFullscreen) {
-       document.msExitFullscreen();
-     } else if (document.mozCancelFullScreen) {
-       document.mozCancelFullScreen();
-     } else if (document.webkitExitFullscreen) {
-       document.webkitExitFullscreen();
-     }
- }  
+
